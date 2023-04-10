2fee872eaf9ef11a962cf010053fb5c8aa355261
Rystambek <rustambekjumaqulov@gmail.com>
2023-04-10 12:53:31 +0500

    admin panel

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telegram import Update,ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardButton,InlineKeyboardMarkup
 from telegram.ext import Updater, Filters, CallbackContext, MessageHandler, CallbackQueryHandler, CommandHandler
-from handler import start,tekshir,download
+from handler import start,tekshir,download,admin
 import os
 
 TOKEN = "6004154698:AAEo2pZT8WqoCqRGAXZchoYMtdgozcq3Kbc"
@@ -10,6 +10,7 @@ updater = Updater(token=TOKEN)
 dp = updater.dispatcher
 dp.add_handler(CommandHandler('start',start))
 dp.add_handler(CallbackQueryHandler(tekshir,pattern='tekshirish'))
+dp.add_handler(MessageHandler(Filters.text("👤Admin panel"),admin))
 dp.add_handler(MessageHandler(Filters.update,download))
 updater.start_polling()
 updater.idle()
