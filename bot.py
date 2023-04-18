9dcae2f654797dc7e7927f3c7963c9a56e730a89
Rystambek <rustambekjumaqulov@gmail.com>
2023-04-18 13:39:50 +0500

    reklama

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telegram import Update,ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardButton,InlineKeyboardMarkup
 from telegram.ext import Updater, Filters, CallbackContext, MessageHandler, CallbackQueryHandler, CommandHandler
-from handler import start,tekshir,download,admin,back_admin
+from handler import start,tekshir,download,admin,back_admin,reklama,rek_query
 import os
 
 TOKEN = "6004154698:AAEo2pZT8WqoCqRGAXZchoYMtdgozcq3Kbc"
@@ -10,8 +10,10 @@ updater = Updater(token=TOKEN)
 dp = updater.dispatcher
 dp.add_handler(CommandHandler('start',start))
 dp.add_handler(CallbackQueryHandler(tekshir,pattern='tekshirish'))
-dp.add_handler(MessageHandler(Filters.text("🔐 Admin"),admin))
+dp.add_handler(MessageHandler(Filters.text("🔐 Admin") | Filters.text("🔚 Chiqish"),admin))
 dp.add_handler(MessageHandler(Filters.text("🔚 Admindan Chiqish"),back_admin))
+dp.add_handler(MessageHandler(Filters.text("🔖 Reklama"),reklama))
+dp.add_handler(CallbackQueryHandler('rek',rek_query))
 dp.add_handler(MessageHandler(Filters.update,download))
 updater.start_polling()
 updater.idle()
